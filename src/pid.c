f5b13ca9d64f60adde1cb11bc1f89307f2046045
JonasM <jonasmenge@shaw.ca>
2016-01-13 22:38:29 -0800

    Added comt explaining the pid file cuz I dint save

---
+++ src/pid.c
@@ -0,0 +1,6 @@
+/*  We may need to make this a .h file...
+
+    I think it would be good to have a seperate pid code that takes the
+    variables from the other functions and does the number crunching... idk
+
+*/
