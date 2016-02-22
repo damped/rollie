8e6db9c798e4a14afe15bbebfd1bce2179dc854c
damped <damped@users.noreply.github.com>
2016-02-21 19:28:53 -0800

    Getting the angle from the IMU

--- src/getangle.c
+++ src/getangle.c
@@ -1,11 +1,19 @@
 //this code was edited from the toilet
 
+#include <iostream>
+#include <errno.h>
+
 #include "stdio.h"
 #include <math.h>
 #include <wiringPiI2c.h>
 
 void main() {
+	
+	int fd, result;
+	
+	fd = wiringPiI2CSetup(0x60);
 
+   	cout << "Init result: "<< fd << endl;
 	printf("helloworld!\n");
 
 }
