a00908150492a37607a3281f905a6d976585c0f2
damped <damped@users.noreply.github.com>
2016-02-26 15:56:37 -0800

    Reading data from the IMU

--- src/getangle.c
+++ src/getangle.c
@@ -9,12 +9,14 @@
 
 int main() {
 
-        int fd, result;
+        int fd, data;
 
-        fd = wiringPiI2CSetup(0x60);
-
-        //cout << "Init result: "<< fd << endl;
-        printf("fd:%d",fd);
+        fd = wiringPiI2CSetup(0x68);
+        
+        data = int wiringPiI2CReadReg16 (fd, 0) ;
+        
+        printf("fd:%d", fd);
+        printf("Data:%d", data);
         return fd;
 }
 
