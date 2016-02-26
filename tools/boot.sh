2d5a7c66d18397eb56352647a7f1243241e4c154
damped <damped@users.noreply.github.com>
2016-02-26 11:12:26 -0800

    Allowed logging boot messages

--- tools/boot.sh
+++ tools/boot.sh
@@ -4,14 +4,15 @@
 #
 #   Runs at boot
 
-cd /home/pi/rollie/tools/ > /dev/null 2>&1
+cd /home/pi/rollie/tools/ #> /dev/null 2>&1
 
 # Send IP with email
-/usr/bin/python /home/pi/rollie/tools/sendip.py > /dev/null 2>&1 &&
+/usr/bin/python /home/pi/rollie/tools/sendip.py && #> /dev/null 2>&1 &&
 
 # Connect to the server
-/bin/bash connect_to_server.sh > /dev/null 2>&1 &&
+/bin/bash connect_to_server.sh && #> /dev/null 2>&1 &&
 
 # Get the newest version of the code
-runuser -l  pi -c '/bin/bash update.sh > /dev/null 2>&1'
+#runuser -l  pi -c '/bin/bash update.sh > /dev/null 2>&1'
 
+sh update.sh
