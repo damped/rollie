816e5851c2ee9d5445db71af0715db0dbd8000c2
damped <damped@users.noreply.github.com>
2016-02-27 13:33:13 -0800

    Testing boot.sh

--- tools/boot.sh
+++ tools/boot.sh
@@ -4,13 +4,13 @@
 #
 #   Runs at boot
 
-cd /home/pi/rollie/tools/ #> /dev/null 2>&1
+#cd /home/pi/rollie/tools/ #> /dev/null 2>&1
 
 # Send IP with email
-/usr/bin/python /home/pi/rollie/tools/sendip.py \& #> /dev/null 2>&1 &&
+/usr/bin/python /home/pi/rollie/tools/sendip.py & #> /dev/null 2>&1 &&
 
 # Connect to the server
-/bin/bash connect_to_server.sh & #> /dev/null 2>&1 &&
+sh /home/pi/rollie/tools/connect_to_server.sh & #> /dev/null 2>&1 &&
 
 # Get the newest version of the code
 #runuser -l  pi -c '/bin/bash update.sh > /dev/null 2>&1'
