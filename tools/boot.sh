bf26d65ace427b2bee4e9d666fc4937b83e404c0
damped <damped@users.noreply.github.com>
2016-02-27 21:28:07 +0000

    fixing boot.sh

--- tools/boot.sh
+++ tools/boot.sh
@@ -7,12 +7,13 @@
 cd /home/pi/rollie/tools/ #> /dev/null 2>&1
 
 # Send IP with email
-/usr/bin/python /home/pi/rollie/tools/sendip.py && #> /dev/null 2>&1 &&
+/usr/bin/python /home/pi/rollie/tools/sendip.py \& #> /dev/null 2>&1 &&
 
 # Connect to the server
-/bin/bash connect_to_server.sh && #> /dev/null 2>&1 &&
+/bin/bash connect_to_server.sh & #> /dev/null 2>&1 &&
 
 # Get the newest version of the code
 #runuser -l  pi -c '/bin/bash update.sh > /dev/null 2>&1'
 
-sh update.sh
+sh /home/pi/rollie/tools/update.sh
+
