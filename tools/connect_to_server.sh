2bafb24ceb7813273d5126d076068e261298cf9a
damped <jonasmenge@shaw.ca>
2016-02-28 06:18:46 +0000

    made connect_to_server.sh connect more often

--- tools/connect_to_server.sh
+++ tools/connect_to_server.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 createTunnel() {
-  /usr/bin/ssh -N -R $(hostname):localhost:22 root@159.203.6.212
+  sleep 2
+  /usr/bin/ssh -N -R $(hostname):localhost:22 root@159.203.6.212 &
   if [[ $? -eq 0 ]]; then
     echo Tunnel to jumpbox created successfully
   else
     echo An error occurred creating a tunnel to jumpbox. RC was $?
   fi
 }
-/bin/pidof ssh
-if [[ $? -ne 0 ]]; then
-  echo Creating new tunnel connection
-  createTunnel
-fi
+
+for i in `seq 1 5`;
+do
+  /bin/pidof ssh
+  if [[ $? -ne 0 ]]; then
+    echo Creating new tunnel connection
+    createTunnel
+  fi
+echo ye
+sleep 5
+done
