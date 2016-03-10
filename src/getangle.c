668bbe8b472586a37efc4706b10ea3610f5e5a25
mikeosynthesis <mikeosynthesis@outlook.com>
2016-03-10 15:46:23 -0800

    edited the angle code

--- src/getangle.c
+++ src/getangle.c
@@ -1,5 +1,9 @@
 
 //authors Michael and Jonas
+//description, Program to return an accurate angle from the IMU.
+//
+//Outputs
+
 
 #include <iostream>
 #include <errno.h>
@@ -7,6 +11,13 @@
 #include "stdio.h"
 #include <math.h>
 #include <wiringPiI2C.h>
+
+/*
+ //function prototypes
+void accelGetc0ord(void);   //grabs the  coordinates and from accelerometer
+void gyroGetcoord(void);    //grabs the coordinates from gyro
+void imuSetup(void);        //setup
+*/
 
 int main() {
 
@@ -20,18 +31,23 @@ int main() {
         wiringPiI2CWriteReg8(devGyro, 0x15, 0x09);
         wiringPiI2CWriteReg8(devGyro, 0x16, 0x1a);
 
-
 	printf("Gyro Data:\n");
 
-        for (int i = 0; i<=5; i++)
+        for (int i = 0; i<=3; i++)
         {
-                printf("0x%x: %x\n", i+0x1d, wiringPiI2CReadReg8(devGyro,(0x1d+i)));
+                int a = i*2;
+                double angles = wiringPiI2CReadReg16(devGyro,(0x1d+a));
+                printf("%x: %lf\n", i+0x1d, angles);
         }
 
+//      accelerometer code
+/*
         int devAccel = wiringPiI2CSetup(0x53);
         int dataAccel = wiringPiI2CReadReg8(devAccel,0x00);
 
         printf("\nAccel device:%x\n", devAccel);
         printf("Accel device id:%x\n", dataAccel);
+*/
+
         return 0;
 }
