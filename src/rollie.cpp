e082db155a5c1e92c68b90b057622389865b7a3f
damped <damped@users.noreply.github.com>
2016-03-10 17:30:11 -0800

    Testing multithread ability of the pi 1

--- src/rollie.cpp
+++ src/rollie.cpp
@@ -13,5 +13,12 @@
 #include <thread>
 //#include <wiringPi.h>
 
-
-
+int main()
+{
+  using namespace std;
+  unsigned int n = thread::hardware_concurrency();
+  cout << n << " concurrent threads are supported.\n";
+  
+  
+  return 0;
+}
