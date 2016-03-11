04ce0ef66b5c074a2e29ee6e57e9f3f50bdeeb40
damped <damped@users.noreply.github.com>
2016-03-10 17:22:56 -0800

    Create rollie.cpp

---
+++ src/rollie.cpp
@@ -0,0 +1,17 @@
+/*************************************************************
+ *
+ *  rollie.cpp - Self balencing robot project
+ *
+ *  Authours: Jonas Menge and Michael Jones
+ *
+ *  Usage:
+ *    
+ *
+ *************************************************************/
+
+#include <stdio.h>
+#include <thread>
+//#include <wiringPi.h>
+
+
+
