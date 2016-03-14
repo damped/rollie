21c01ef6e340d3fd607f3127c3e37b5bc938e624
mikeosynthesis <mikeosynthesis@outlook.com>
2016-03-14 15:20:32 -0700

    debug

--- src/getangle.c
+++ src/getangle.c
@@ -201,7 +201,7 @@ void ComplementaryFilter(int accDev, int devGyro, float *pitch, float *roll)
   //   printf("hex acc X: %x, Y: %x, Z: %x\n",aX,aY,aZ);
 
      aPitch = (atan2(X,sqrt(Y*Y+Z*Z)) * 180.0) / PI;
-     printf("%lf,%lf,%lf\n",X,Y,Z );
+     //printf("%lf,%lf,%lf\n",X,Y,Z );
   //   printf("pitch = %f\n",aPitch);
 
 
