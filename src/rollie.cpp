e8d96cd50df61c46576f8aff7d8475e396ff0392
damped <jonasmenge@shaw.ca>
2016-03-27 04:33:57 +0000

    added some edits from the PI

--- src/rollie.cpp
+++ src/rollie.cpp
@@ -13,7 +13,7 @@
 #include "imu.h"
 
 //#include <thread>
-//#include <wiringPi.h>
+#include <wiringPi.h>
 
 //#include "getangle.c"
 #include "pid.h"
@@ -26,7 +26,7 @@ void loop(pid_filter_t *pid, int devAccel, int devGyro);
 int main()
 {
     using namespace std;
-
+	wiringPiSetupGpio();
 
     /* PID controller setup */
     pid_filter_t pid;
@@ -37,7 +37,7 @@ int main()
     int devAccel = accConfig();
     int devGyro = gyroConfig();
 
-
+    pinMode (17, OUTPUT);
 
     loop(&pid, devAccel, devGyro);
 
@@ -49,11 +49,17 @@ void loop(pid_filter_t *pid, int devAccel, int devGyro)
 {
     float error;
     float setpoint = 0;
+	
 
-    while (1) {
-        float current = getAngle(devAccel, devGyro);
-        error = setpoint - current;
-        float pidOutput = pid_process(pid, error);
-        printf("\rSetPnt = %f, Current = %f, Error = %f, PIDout = %f", setpoint, current, error, pidOutput);
+	while(1){
+    //for (int i = 0; i < 1000; i++) {
+        //float current = 99;//getAngle(devAccel, devGyro);
+        //error = setpoint - current;
+        //float pidOutput = pid_process(pid, error);
+        //printf("\rSetPnt = %f, Current = %f, Error = %f, PIDout = %f", setpoint, current, error, pidOutput);
+	digitalWrite (17, LOW);
+	//delay(5);
+	digitalWrite (17, HIGH);
+	//delay(5);
     }
 }
