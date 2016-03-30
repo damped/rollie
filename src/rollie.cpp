f2cd59efba80d56d1bb6133b9a308e9afa493182
Damped <damped@users.noreply.github.com>
2016-03-29 20:06:07 -0700

    trying to get it to compile some more

--- src/rollie.cpp
+++ src/rollie.cpp
@@ -43,7 +43,7 @@ int main()
     printf("one\n");
     pthread_t stepperControll;
     printf("2\n");
-    void *stepperControll();
+    //void *stepperControll();
     printf("3\n");
     float rate = 0;
     printf("4\n");
