11d4de47df6b76c8c4bf2acc292aaabf909787c2
JonasM <jonasmenge@shaw.ca>
2016-04-02 16:49:52 -0700

    Added #defines

--- src/stepper.h
+++ src/stepper.h
@@ -5,13 +5,20 @@
 #include <wiringPi.h>
 #include <math.h>
 
+//constants
+#define MOTOR_STEPS 400.0   // Full steps per rotation of the motor
+#define MICRO_STEPS 0.5     // Micro steps (half steps = 0.5)
+#define DIA 0.085           // Daiameter in m
+
+#define PI  3.14159265359 // Its pi day... need at least 11 decimals
+
+
+
 // function prototypes
 void motorSetup();      // Initialize motor pins
 void wait(float time);  // Wait loop
 void stepperControl(float *period);
 void setSpeed(float velocity, float *pulseTimePtr);
 
-//constants
-#define PI  3.14159265359 // Its pi day... need at least 11 decimals
 
 #endif
